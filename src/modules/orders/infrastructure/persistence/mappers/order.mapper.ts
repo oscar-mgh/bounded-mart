584000803fa54dd455ec0a06ac6e939ec6d552e9
oscar galindo <oscar.galindo1@yahoo.com>
2026-01-21 08:45:53 -0600

    refactor: order and user mappers for responses

--- src/modules/orders/infrastructure/persistence/mappers/order.mapper.ts
+++ src/modules/orders/infrastructure/persistence/mappers/order.mapper.ts
@@ -2,6 +2,7 @@ import { Id } from 'src/modules/shared/domain/value-objects/id.vo';
 import { Order } from '../../../domain/entities/order.entity';
 import { OrderItem } from '../../../domain/value-objects/order-item.vo';
 import { OrderDocument } from '../schemas/order.schema';
+import { OrderResponseDto } from '../../http/dtos/order-response.dto';
 
 export class OrderMapper {
   static toDomain(doc: OrderDocument): Order {
@@ -10,7 +11,7 @@ export class OrderMapper {
     return new Order(Id.create(doc._id.toString()), doc.customerId, items, doc.status as any, new Date(doc.createdAt));
   }
 
-  static toPersistence(order: Order) {
+  static toPersistence(order: Order): any {
     return {
       _id: order.id.getValue(),
       customerId: order.getCustomerId(),
@@ -24,4 +25,22 @@ export class OrderMapper {
       totalAmount: order.getTotalAmount(),
     };
   }
+
+  static toResponse(order: Order): OrderResponseDto {
+    return {
+      id: order.id.getValue(),
+      customerId: order.getCustomerId(),
+      status: order.getStatus(),
+      totalAmount: order.getTotalAmount(),
+      createdAt: order.getCreatedAt(),
+      items: order.getItems().map(item => ({
+        productId: item.getProductId(),
+        name: item.getProductName(),
+        price: item.getUnitPrice(),
+        quantity: item.getQuantity(),
+        subtotal: item.getUnitPrice() * item.getQuantity(),
+      })),
+    };
+  }
 }
+
--- src/modules/users/infrastructure/persistance/mappers/user.mapper.ts
+++ src/modules/users/infrastructure/persistance/mappers/user.mapper.ts
@@ -1,3 +1,4 @@
+import { Types } from 'mongoose';
 import { User } from 'src/modules/users/domain/entities/user.entity';
 import { Email } from 'src/modules/users/domain/value-objects/email.vo';
 import { Id } from '../../../../shared/domain/value-objects/id.vo';
@@ -5,19 +6,12 @@ import { UserDocument } from '../entities/user.schema';
 
 export class UserMapper {
   static toDomain(raw: UserDocument): User {
-    return new User(
-      new Id(raw._id.toString()),
-      raw.username,
-      new Email(raw.email),
-      raw.password,
-      raw.role,
-      raw.active,
-    );
+    return new User(new Id(raw._id.toString()), raw.username, new Email(raw.email), raw.password, raw.role, raw.active);
   }
 
-  static toPersistence(domain: User): any {
+  static toPersistence(domain: User): Partial<UserDocument> {
     return {
-      _id: domain.id.getValue(),
+      _id: new Types.ObjectId(domain.id.getValue()),
       username: domain.getUsername(),
       email: domain.getEmail().getValue(),
       password: domain.getPassword(),
